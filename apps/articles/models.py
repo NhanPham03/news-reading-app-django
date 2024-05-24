67c8dba5f40286888394810b43e4b9514972c43f
NhanPham03 <ph.nhan03@gmail.com>
2024-05-24 11:37:03 +0700

    Fix typos

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
   title = models.CharField(max_length=255, blank=False)
   content = models.TextField(blank=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
-  category = models.CharField(choices=Category.choices(), default=Category.UNCATEROGIZED, max_length=20)
+  category = models.CharField(choices=Category.choices(), default=Category.UNCATEGORIZED, max_length=20)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
